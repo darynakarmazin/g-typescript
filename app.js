d2edefa4dd253c6c89ef3a194b0a1a22ffa5b53b
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-09-29 13:47:02 +0300

    Added resolve for task-2

--- app.js
+++ app.js
@@ -18,3 +18,61 @@ var anything;
 anything = -20;
 anything = "Text";
 anything = {};
+// Виправте код зі змінною unknown, щоб у нього можна було зберегти змінну з текстом.
+var some;
+some = "Text";
+var str;
+if (typeof some === "string") {
+    str = some;
+}
+// Зробіть незмінний масив із суворо описаними типами. Масив для прикладу.
+var person;
+person = ["Max", 21];
+// Опишіть enum умову наступну: він повинен відображати статус завантаження. Завантажується (LOADING) та завантажена (READY).
+var Load;
+(function (Load) {
+    Load[Load["LOADING"] = 0] = "LOADING";
+    Load[Load["READY"] = 1] = "READY";
+})(Load || (Load = {}));
+var page = {
+    load: Load.READY,
+};
+if (page.load === Load.LOADING) {
+    console.log("Сторінка завантажується");
+}
+if (page.load === Load.READY) {
+    console.log("Сторінка завантажена");
+}
+// Зробіть змінну, яка може приймати або рядок, або число.
+var union;
+union = "Text";
+union = 10;
+// Зробіть змінну, яка може приймати лише одне значення з двох: 'enable' або 'enable'
+var literal;
+literal = "enable";
+// Вкажіть типи для наступних функцій
+function showMessage(message) {
+    console.log(message);
+}
+function calc(num1, num2) {
+    return num1 + num2;
+}
+function customError() {
+    throw new Error("Error");
+}
+var page1 = {
+    title: "The awesome page",
+    likes: 100,
+    accounts: ["Max", "Anton", "Nikita"],
+    status: "open",
+    details: {
+        createAt: new Date("2021-01-01"),
+        updateAt: new Date("2021-05-01"),
+    },
+};
+var page2 = {
+    title: "Python or Js",
+    likes: 5,
+    accounts: ["Alex"],
+    status: "close",
+};
--- app.ts
+++ app.ts
@@ -21,9 +21,80 @@ anything = "Text";
 anything = {};
 
 // Виправте код зі змінною unknown, щоб у нього можна було зберегти змінну з текстом.
-// let some: unknown;
-// some = "Text";
+let some: unknown;
+some = "Text";
 
-// let str: string;
+let str: string;
 
-// str = some;
+if (typeof some === "string") {
+  str = some;
+}
+
+// Зробіть незмінний масив із суворо описаними типами. Масив для прикладу.
+let person: [string, number];
+person = ["Max", 21];
+
+// Опишіть enum умову наступну: він повинен відображати статус завантаження. Завантажується (LOADING) та завантажена (READY).
+enum Load {
+  LOADING,
+  READY,
+}
+const page = {
+  load: Load.READY,
+};
+if (page.load === Load.LOADING) {
+  console.log("Сторінка завантажується");
+}
+if (page.load === Load.READY) {
+  console.log("Сторінка завантажена");
+}
+// Зробіть змінну, яка може приймати або рядок, або число.
+let union: string | number;
+union = "Text";
+union = 10;
+// Зробіть змінну, яка може приймати лише одне значення з двох: 'enable' або 'enable'
+let literal: "enable" | "enable";
+literal = "enable";
+// Вкажіть типи для наступних функцій
+
+function showMessage(message): void {
+  console.log(message);
+}
+
+function calc(num1, num2): number {
+  return num1 + num2;
+}
+
+function customError(): never {
+  throw new Error("Error");
+}
+
+// Створіть свій тип даних на основі наявних даних.
+type PageType = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+};
+
+const page1 = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
+
+const page2 = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
